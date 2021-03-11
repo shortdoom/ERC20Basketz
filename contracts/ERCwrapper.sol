bf151b853c0cce8b84490719f0b7174046e46779
neotokio <oskar.milosz.wojciechowski@gmail.com>
2021-03-11 23:03:03 +0100

    0.0.4 PoC, basic functionality, rough edges

--- contracts/ERCwrapper.sol
+++ contracts/ERCwrapper.sol
@@ -6,75 +6,22 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/EnumerableMap.sol";
-import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 
 contract ercWrapper is ERC721 {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
     using Counters for Counters.Counter;
 
-    Counters.Counter private _WrapIndex;
+    Counters.Counter private _wrapID;
 
-    mapping(address => UserIndex) wrapped;
-    mapping(address => UserMap[]) mapped;
-
-    struct UserMap {
-        uint256 id;
-        address[] tokens;
-        uint256[] amounts;
-        bool wrapOwner;
-    }
+    mapping(address => mapping (uint256 => UserIndex)) private wrapped; 
 
     struct UserIndex {
-        uint256 id;
         address[] tokens;
         uint256[] amounts;
-        bool wrapOwner;
-    }
-
-    modifier onlyWrapOwner(){
-        require(wrapped[msg.sender].wrapOwner);
-        _;
     }
 
     constructor() ERC721("WrappedIndex", "WRAP") {}
-
-    // Two options: Non-Fungible 721 (Transfer only whole amount), Fungible 1155 (Transfer parts). Second requires additional logic.
-
-    function wrapperMapping(address[] memory tokens, uint256[] memory amounts) external returns (uint256) {
-        for (uint256 i = 0; i < tokens.length; i++) {
-            bool success = IERC20(tokens[i]).transferFrom(msg.sender, address(this), amounts[i]);
-            require(success);
-        }
-
-        _WrapIndex.increment();
-        uint256 wrapId = _WrapIndex.current();
-        mapped[msg.sender].push(UserMap({id: wrapId, tokens: tokens, amounts: amounts, wrapOwner: true}));
-        _mint(msg.sender, wrapId);
-        // _setTokenURI(wrapId, "NFT-Location"); // Additional URI data can go here
-
-        return wrapId;
-    }
-
-    function unwrapperMapping(uint256 _basketId) external onlyWrapOwner {
-        // Lets say msg.sender owns basketId 1 & 2, but wants to unwrap only 2
-        uint256 basketNo = ERC721.tokenOfOwnerByIndex(msg.sender, _basketId); // return given msg.sender basket
-
-        for (uint256 i = 0; i < mapped[msg.sender][_basketId].tokens.length; i++) {
-            IERC20(mapped[msg.sender][_basketId].tokens[i]).approve(address(this), mapped[msg.sender][_basketId].amounts[i]);
-            bool success =
-                IERC20(mapped[msg.sender][_basketId].tokens[i]).transferFrom(
-                    address(this),
-                    msg.sender,
-                    mapped[msg.sender][_basketId].amounts[i]
-                );
-            require(success);
-        }
-
-        delete mapped[msg.sender][_basketId];
-        _burn(basketNo);
-    }
 
     function wrapper(address[] memory tokens, uint256[] memory amounts) external returns (uint256) {
         for (uint256 i = 0; i < tokens.length; i++) {
@@ -82,50 +29,46 @@ contract ercWrapper is ERC721 {
             require(success);
         }
 
-        _WrapIndex.increment();
-        uint256 wrapId = _WrapIndex.current();
-        wrapped[msg.sender] = UserIndex({ id: wrapId, tokens: tokens, amounts: amounts, wrapOwner: true });
+        // NOTE: Probably there is some better way to manage wrapId/tokenId
+        _wrapID.increment();
+        uint256 wrapId = _wrapID.current();
+        wrapped[msg.sender][wrapId] = UserIndex({tokens: tokens, amounts: amounts});
         _mint(msg.sender, wrapId);
-        // _setTokenURI(wrapId, "NFT-Location"); // Additional URI data can go here
+        // NOTE: URI. Maybe off-chain stats, lightweight analysis of basket price change etc.
 
         return wrapId;
     }
 
-    function unwrapper(uint256 _basketId) external onlyWrapOwner {
-        // Lets say msg.sender owns basketId 1 & 2, but wants to unwrap only 2
-        uint256 basketNo = ERC721.tokenOfOwnerByIndex(msg.sender, _basketId); // return given msg.sender basket
-        for (uint256 i = 0; i < wrapped[msg.sender].tokens.length; i++) {
-            IERC20(wrapped[msg.sender].tokens[i]).approve(address(this), wrapped[msg.sender].amounts[i]);
+    function unwrapper(uint256 _wrapId) external {
+        require(ERC721.ownerOf(_wrapId) == msg.sender, "Not an owner of a basket");
+        for (uint256 i = 0; i < wrapped[msg.sender][_wrapId].tokens.length; i++) {
+            IERC20(wrapped[msg.sender][_wrapId].tokens[i]).approve(address(this), wrapped[msg.sender][_wrapId].amounts[i]);
             bool success =
-                IERC20(wrapped[msg.sender].tokens[i]).transferFrom(
+                IERC20(wrapped[msg.sender][_wrapId].tokens[i]).transferFrom(
                     address(this),
                     msg.sender,
-                    wrapped[msg.sender].amounts[i]
+                    wrapped[msg.sender][_wrapId].amounts[i]
                 );
             require(success);
         }
 
-        delete wrapped[msg.sender];
-        _burn(basketNo);
+        delete wrapped[msg.sender][_wrapId];
+        _burn(_wrapId);
     }
 
-    /**
-    Override ERC721 functions to keep track of global storage variables.
-    _transfer is modified to also remove user baskets upon change of ownership
-     */
     function _transfer(
         address from,
         address to,
-        uint256 tokenId
-    ) internal onlyWrapOwner override {
-        require(ERC721.ownerOf(tokenId) == from, "ERC721: transfer of token that is not own"); // internal owner
+        uint256 _wrapId
+    ) internal override {
+        require(ERC721.ownerOf( _wrapId) == from, "ERC721: transfer of token that is not own"); // internal owner
         require(to != address(0), "ERC721: transfer to the zero address");
-        super._transfer(from, to, tokenId);
-        wrapped[to] = wrapped[msg.sender]; // Copy UserIndex (token balance) from msg.sender (owner) to `to` (receiver)
-        delete wrapped[msg.sender]; // delete UserIndex of original owner
+        super._transfer(from, to,  _wrapId);
+        wrapped[to][ _wrapId] = wrapped[msg.sender][ _wrapId]; // Copy UserIndex (token balance) from msg.sender (owner) to `to` (receiver)
+        delete wrapped[msg.sender][_wrapId]; // delete UserIndex of original owner
     }
 
-    function wrappedBalance(uint256 _tokenId)
+    function wrappedBalance(uint256  _wrapId)
         public
         view
         returns (
@@ -134,7 +77,8 @@ contract ercWrapper is ERC721 {
             uint256[] memory amounts
         )
     {
-        address owner = ERC721.ownerOf(_tokenId);
-        return (wrapped[owner].id, wrapped[owner].tokens, wrapped[owner].amounts);
+        address owner = ERC721.ownerOf( _wrapId);
+        return ( _wrapId, wrapped[owner][ _wrapId].tokens, wrapped[owner][ _wrapId].amounts);
     }
+
 }
